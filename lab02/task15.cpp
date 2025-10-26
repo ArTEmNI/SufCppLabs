9ce127aa1aa804c66e00eb73dbedd0a1d31c8fb9
coroutin <auhnev29@gmail.com>
2025-10-26 20:53:39 +0300

    suf

--- lab02/task15.cpp
+++ lab02/task15.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+#include <ctime>
+
+int main() {
+    long tmday_inp, tmmon_inp, tmyear_inp;
+
+    std::cin >> tmyear_inp, tmmon_inp, tmday_inp;
+
+    std::time_t now = std::time(nullptr);
+    std::tm * local = std::localtime(&now);
+
+    if ((local->tm_year + 1900) - tmyear_inp >= 18) {
+        int o = ((local->tm_year + 1900) - tmyear_inp) + ((local->tm_mon + 1) - tmmon_inp) + (local->tm_mday - tmday_inp);
+        std::cout << o;
+        std::cout << (local->tm_year + 1900) - tmyear_inp << std::endl;
+        std::cout << (local->tm_mon + 1) - tmmon_inp << std::endl;
+        std::cout << local->tm_mday - tmday_inp << std::endl;
+    }
+
+    return 0;
+}
